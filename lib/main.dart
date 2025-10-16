bc012e2fb3e28d6f46914007e406afdf9897fe4c
smillwvy <171736343+smillwvy@users.noreply.github.com>
2025-10-17 00:24:07 +0100

    Add class Order Item Display

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,8 @@ Widget build(BuildContext context) {
   );
 }
 
+
+
 // class MyApp extends StatelessWidget {
 //   const MyApp({super.key});
 
@@ -166,4 +168,16 @@ Widget build(BuildContext context) {
 //   }
 // }
 
+}
+
+class OrderItemDisplay extends StatelessWidget {
+  final String itemType;
+  final int quantity;
+
+  const OrderItemDisplay(this.quantity, this.itemType, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('This is a placeholder for OrderItemDisplay');
+  }
 }
